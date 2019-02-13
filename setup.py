b6f495d963f2aa74b73df3623f2f15ccfc8a935e
Stephan Chang <stephan.chang@flowef.com>
2019-02-13 19:27:03 -0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dragonfly-sync",
+    name="dragonfly-sync-lib",
     version="0.1.0",
     author="Stephan Chang",
-    author_email="stchepanhagn@gmail.com",
-    description="A library that facilitates transporting data around",
+    author_email="stephan.chang@flowef.com",
+    description="A library that facilitates transporting data around.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/stchepanhagn/dragonfly",
+    url="https://github.com/flowef/dragonfly-sync-lib",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
