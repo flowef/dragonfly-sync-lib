54658a1379503e4da2b68bb3b520522c468c232e
Stephan Chang <stephan.chang@flowef.com>
2019-02-14 14:34:04 -0200

    Added name argument to config generator

--- gen_config.py
+++ gen_config.py
@@ -28,18 +28,18 @@ EXAMPLE_FILENAME = 'example.yaml'
 DEFAULT_FILENAME = 'sync.yaml'
 
 
-def gen_sample_sync_config():
+def gen_sample_sync_config(filename=DEFAULT_FILENAME):
     logging.info("Generating new config from example.yaml")
     try:
         with open(EXAMPLE_FILENAME) as example:
-            with open(DEFAULT_FILENAME, 'w') as copy:
+            with open(filename, 'w') as copy:
                 copy.write(example.read())
     except FileNotFoundError:
         logging.error(
             "Example file was deleted! Cannot copy sample configuration.")
         raise
 
-    logging.info(f"Done! Written to '{DEFAULT_FILENAME}'")
+    logging.info(f"Done! Written to '{filename}'")
 
 
 if __name__ == "__main__":
