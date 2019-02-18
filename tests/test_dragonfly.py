0c60cdad6605623eb49e97561f963036d604b6e8
Stephan Chang <stephan.chang@flowef.com>
2019-02-18 16:32:20 -0300

    Added tests

--- tests/test_dragonfly.py
+++ tests/test_dragonfly.py
@@ -1,26 +1,16 @@
+import json
+
 from dragonfly import dragonfly
 
-single_result = [{'id': 1, 'value': 'ok', 'deleted': False}]
-double_result = [{
-    'id': 1,
-    'value': 'ok',
-    'deleted': True
-}, {
-    'id': 2,
-    'value': 'ok',
-    'deleted': False
-}]
 unexpected_result = [{'id': 1, 'value': 'not ok'}]
 
 
 class DummyDataSource(dragonfly.DataSourceAdapter):
     def fetch(self, entity, metadata):
-        if entity == "TestEntity":
-            yield single_result
-        elif entity == "OtherEntity":
-            yield double_result
-        else:
-            yield unexpected_result
+        with open('tests/testbase.json') as first:
+            yield json.load(first)
+        with open('tests/update.json') as update:
+            yield json.load(update)
 
     def close(self):
         pass
@@ -29,26 +19,20 @@ class DummyDataSource(dragonfly.DataSourceAdapter):
 class DummyDB():
     db = {}
 
-    def upsert(self, id, value):
+    def upsert(id, value):
         DummyDB.db[id] = value
 
-    def remove(self, id):
+    def remove(id):
         if id in DummyDB.db:
             del DummyDB.db[id]
 
-    def __getitem__(self, key):
-        return DummyDB.db.get(key)
-
 
 class DummyPersistence(dragonfly.PersistenceAdapter):
-    def __init__(self):
-        self.client = DummyDB()
-
     def upsert(self, collection, record, metadata):
-        self.client.upsert(record['id'], record['value'])
+        DummyDB.upsert(record['id'], record['value'])
 
     def remove(self, collection, record, metadata=None):
-        self.client.remove(record['id'])
+        DummyDB.remove(record['id'])
 
     def close(self):
         pass
@@ -57,26 +41,14 @@ class DummyPersistence(dragonfly.PersistenceAdapter):
 def test__reader_calls_underlying_adapter():
     data_reader = dragonfly.DefaultDataReader(DummyDataSource())
 
-    result = next(data_reader.read('TestEntity', {}))
+    reads = data_reader.read('_', {})
+    result = next(reads)
 
-    assert result[0]['id'] == single_result[0]['id']
+    assert result[0]['id'] == 1
     assert result[0]['value'] == 'ok'
 
 
-def test__underlying_adapter_filters_entities_accordingly():
-    data_reader = dragonfly.DefaultDataReader(DummyDataSource())
-
-    result = next(data_reader.read('OtherEntity', {}))
-
-    assert len(result) == len(double_result)
-
-    for i in range(len(result)):
-        assert result[i]['id'] == (i + 1) and result[i]['value'] == 'ok'
-
-
 def test__writer_calls_upsert_on_soft_delete():
-    db = DummyDB()
-
     metadata = {
         'soft_delete': True,
         'table': 'TestEntity',
@@ -84,21 +56,20 @@ def test__writer_calls_upsert_on_soft_delete():
     }
     data_reader = dragonfly.DefaultDataReader(DummyDataSource())
     data_writer = dragonfly.DefaultDataWriter(DummyPersistence())
-    records = next(data_reader.read('TestEntity', {}))
-    data_writer.update('TestEntity', metadata, records)
+    reads = data_reader.read('_', {})
+    records = next(reads)
+    data_writer.update('_', metadata, records)
 
-    assert db[1] == 'ok'
+    assert DummyDB.db.get(1) == 'ok'
 
-    records = next(data_reader.read('OtherEntity', {}))
-    data_writer.update('TestEntity', metadata, records)
+    records = next(reads)
+    data_writer.update('_', metadata, records)
 
-    assert db[1] == 'ok'
-    assert db[2] == 'ok'
+    assert DummyDB.db.get(1) == 'ok'
+    assert DummyDB.db.get(2) == 'ok'
 
 
 def test__writer_calls_remove_on_hard_delete():
-    db = DummyDB()
-
     metadata = {
         'soft_delete': False,
         'table': 'TestEntity',
@@ -106,13 +77,26 @@ def test__writer_calls_remove_on_hard_delete():
     }
     data_reader = dragonfly.DefaultDataReader(DummyDataSource())
     data_writer = dragonfly.DefaultDataWriter(DummyPersistence())
-    records = next(data_reader.read('TestEntity', {}))
-    data_writer.update('TestEntity', metadata, records)
+    reads = data_reader.read('_', {})
+    records = next(reads)
+    data_writer.update('_', metadata, records)
 
-    assert db[1] == 'ok'
+    assert DummyDB.db.get(1) == 'ok'
 
-    records = next(data_reader.read('OtherEntity', {}))
-    data_writer.update('TestEntity', metadata, records)
+    records = next(reads)
+    data_writer.update('_', metadata, records)
 
-    assert db[1] is None
-    assert db[2] == 'ok'
+    assert DummyDB.db.get(1) is None
+    assert DummyDB.db.get(2) == 'ok'
+
+
+def test__sync_client_produces_correct_record_count():
+    data_reader = dragonfly.DefaultDataReader(DummyDataSource())
+    data_writer = dragonfly.DefaultDataWriter(DummyPersistence())
+
+    sync = dragonfly.Sync('tests/test.yaml', data_reader, data_writer)
+
+    records = sync()
+
+    assert len(DummyDB.db) == 2
+    assert records == 3
